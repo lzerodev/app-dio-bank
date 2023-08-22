ccc3b390395428ab1d76b1e2962062a33a6dd713
leandro.morais <leandro.morais@fengbrasil.com.br>
2023-08-22 18:29:54 -0300

    Módulo de saque v1

--- app.ts
+++ app.ts
@@ -7,13 +7,14 @@ console.log(peopleAccount)
 
 
 
-peopleAccount.getBalance()
+peopleAccount.getBalance()// Show your current balance
 
 
 const depositAmount = 100; // The amount you want to deposit
 peopleAccount.setDeposit(depositAmount);
 
-// const withdrawAmount = 10; // The amount you want to deposit
-// peopleAccount.setWithdraw(withdrawAmount);
+const withdrawAmount = 150; // The amount you want to withdraw
+peopleAccount.setWithdraw(withdrawAmount);
 
-peopleAccount.getBalance()
+
+peopleAccount.getBalance()// Show your new current balance
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -2,8 +2,8 @@ export abstract class DioAccount {
   private name: string;
   private readonly accountNumber: number;
   private status: boolean = true;
-  deposit?: number
-  withdraw?: number
+  deposit: number = 0
+  withdraw: number = 0
   balance: number = 0;
 
   constructor(name: string, accountNumber: number, balance: number) {
@@ -24,22 +24,22 @@ export abstract class DioAccount {
   setDeposit = (deposit: number): number => {
     if (this.validateStatus()) {
       this.balance = this.balance + deposit
-      console.log("You desposed " + "R$" + deposit + " to your account" );
+      console.log("You deposited " + "R$" + deposit + " to your account" );
     }
     return deposit
   };
 
   setWithdraw = (withdraw: number): number => {
-    if (this.validateStatus() && withdraw > this.balance) {
-      this.balance = this.balance - withdraw;
-      console.log("You withdraw " + "R$" + withdraw);
+    if (this.validateStatus() && withdraw <= this.balance) {
+      this.balance -= withdraw;
+      console.log("You withdrew " + "R$" + withdraw);
       return withdraw;
     }
-    throw new Error('Insufficient funds');
+    throw new Error('Insufficient funds.' + ' R$' + withdraw + ' requested to withdraw');
   };
-
+  
   getBalance = (): void => {
-    console.log("Seu saldo atual é " + this.balance);
+    console.log("Your current balance is " + this.balance);
   };
 
   private validateStatus = (): boolean => {
